9410119263c02e0a7112f159f4168fbf0ec0d27b
Lennart Kobosil <lennartkobosil@gmail.com>
2018-10-20 17:14:37 +0200

    Initial Commit

---
+++ Ambiento/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ambiento
+//
+//  Created by Lennart Kobosil on 20.10.18.
+//  Copyright © 2018 JugendHackt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
